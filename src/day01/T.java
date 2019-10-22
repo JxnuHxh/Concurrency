2899fd2f80d3d1c59db49b512c393f63fa47d378
hxh <617662163@qq.com>
2019-10-22 08:14:39 +0800

    学习并发编程

---
+++ src/day01/T.java
@@ -0,0 +1,18 @@
+package day01;
+/*
+synchronized关键字
+对某个对象加锁
+@author mashibing
+ */
+public class T {
+    private int count = 10;
+    private Object o=new new Object();
+
+    public void m(){
+        synchronized(o){//任何对象要执行下面的代码，必须要拿到o的锁
+            count++;
+            System.out.println(Thread.currentThread().getName()+"count="+count);
+
+        }
+    }
+}
