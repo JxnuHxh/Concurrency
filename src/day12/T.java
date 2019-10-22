f8c709050f40390aea9d4002fe4dcec524e2417b
hxh <617662163@qq.com>
2019-10-22 17:14:42 +0800

    提交

---
+++ src/day12/T.java
@@ -0,0 +1,47 @@
+package day12;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class T {
+
+    int count = 0;
+
+    synchronized void m1() {
+        //do sth need not sync
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count ++;
+
+        //do sth need not sync
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void m2() {
+        //do sth need not sync
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         synchronized(this) {
+            count ++;
+        }
+        //do sth need not sync
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
