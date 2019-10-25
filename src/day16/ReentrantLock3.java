cc7dab1a131dfd0f8b62679dc5bc1347689e9336
hxh <617662163@qq.com>
2019-10-25 18:49:54 +0800

    reenttrantLock

--- src/day16/ReentrantLock3.java
+++ src/day16/ReentrantLock3.java
@@ -1,10 +1,50 @@
 package day16;
 
+import javax.print.attribute.standard.Finishings;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.StreamSupport;
 
 public class ReentrantLock3 {
     Lock lock=new ReentrantLock();
-    void
+    void m1(){
+        try
+        {
+            lock.lock();
+            for(int i=0;i<10;i++){
+                TimeUnit.SECONDS.sleep(1);;
+                System.out.println(i);
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+
+    }
+    void m2(){
+        boolean locked = false;
+        try {
+            locked =lock.tryLock(5,TimeUnit.SECONDS);
+            System.out.println("m2 ..."+locked);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }finally {
+            if(locked)lock.unlock();
+        }
+    }
+    public static void main(String[] args){
+        ReentrantLock3 r1=new ReentrantLock3();
+        new Thread(r1::m1).start();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        new Thread(r1::m2).start();
+    }
+
 
 }
--- src/day16/ReentrantLock5.java
+++ src/day16/ReentrantLock5.java
@@ -1,7 +1,29 @@
 package day16;
 
 
+import sun.applet.Main;
 
-public class ReentrantLock5 {
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLock5 extends Thread {
+    private static ReentrantLock lock=new ReentrantLock(true);
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "获得锁 ");
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantLock5 r1=new ReentrantLock5();
+        Thread th1=new Thread(r1);
+        Thread th2=new Thread(r1);
+        th1.start();
+        th2.start();
+    }
 
 }
